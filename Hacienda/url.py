a8e315ebe5fb335502b8f67002f72c4125f75a1f
Stephano <stephanochang21@gmail.com>
2023-06-05 01:38:32 -0500

    update gitignore x2 xd

--- Hacienda/url.py
+++ Hacienda/url.py
@@ -1,11 +1,10 @@
 from rest_framework import routers
 from django.urls import path
 from .views import ProyectoHaciendaAPIView,LoteAPIView
-from .api import HaciendaViewSet, ProyectoViewSet, LoteViewSet, EstacionViewSet, PlantaViewSet, LecturaViewSet
+from .api import HaciendaViewSet, ProyectoViewSet, EstacionViewSet, PlantaViewSet, LecturaViewSet
 router = routers.DefaultRouter()
 router.register('api/hacienda', HaciendaViewSet)
 router.register('api/proyectos', ProyectoViewSet)
-router.register('api/lotes', LoteViewSet)
 router.register('api/estaciones', EstacionViewSet)
 router.register('api/plantas', PlantaViewSet)
 router.register('api/lecturas', LecturaViewSet)
@@ -13,8 +12,8 @@ router.register('api/lecturas', LecturaViewSet)
 urlpatterns = [
     path('api/hacienda/<int:hacienda_id>/proyectos/', ProyectoHaciendaAPIView.as_view(), name='proyectos-hacienda'),
     # Otras rutas de la aplicación Hacienda
-    path('api/lotesTest/', LoteAPIView.as_view(), name='lotesTest'),
-    path('api/lotesTest/<int:id>', LoteAPIView.as_view(), name='lotesTest'),
-    path('api/lotesTest/details/<int:id>', LoteAPIView.as_view(), name='lotesTest'),
+    path('api/lotes/', LoteAPIView.as_view(), name='lotes'),
+    path('api/lotes/<int:id>/', LoteAPIView.as_view(), name='lotes'),
+    path('api/lotes/details/<int:id>/', LoteAPIView.as_view(), name='lotes'),
 ]
 urlpatterns += router.urls
