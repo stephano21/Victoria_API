283a39968d77df940bd197e0bdf04e0f188cc676
Stephano <stephanochang21@gmail.com>
2024-02-13 01:46:38 -0500

    Get Planta

--- Hacienda/serializer/LecturaSerializer.py
+++ Hacienda/serializer/LecturaSerializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from Hacienda.models import Lectura
 class LecturaSerializers(serializers.ModelSerializer):
+    NombrePlanta = serializers.SerializerMethodField()
     class Meta:
         model = Lectura
         fields = ('__all__')
@@ -9,3 +10,9 @@ class LecturaSerializers(serializers.ModelSerializer):
             field: {'required': True}
             for field in required_fields
         }
+    def get_NombrePlanta(self, instance):
+        # Verificar si la instancia de Produccion tiene un Lote asociado
+        if instance.Id_Planta:
+            # Verificar si el Lote tiene un Proyecto asociado
+            return instance.Id_Planta.Nombre
+        return None  # Manejar el caso en que la relación no existe
